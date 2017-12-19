b401c03cc437835ac8c13be411a8bb2d77690cde
Tom Young <tyoung@n.io>
2017-12-19 12:00:49 -0700

    connection greenbar plus close

--- mssql_block.py
+++ mssql_block.py
@@ -1,12 +1,45 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty
+from nio.properties import VersionProperty, StringProperty, PropertyHolder, ObjectProperty
+import pyodbc
 
+class Credentials(PropertyHolder):
+
+    userid = StringProperty(title='User ID', allow_none=True)
+    password = StringProperty(title="Password", allow_none=True)
 
 class MSSQL(Block):
 
     version = VersionProperty('0.1.0')
+    server = StringProperty(title='Server')
+    database = StringProperty(title='Database')
+    credentials = ObjectProperty(Credentials, title='Connection Credentials')
+
+    def __init__(self):
+        super().__init__()
+        self.cnxn = None
+
+    def configure(self, context):
+        super().configure(context)
+        cnxn_string = (
+            'DRIVER={};'
+            'SERVER={};'
+            'DATABASE={};'
+            'UID={};'
+            'PWD={}')
+        cnxn_string = cnxn_string.format(
+            '{ODBC Driver 13 for SQL Server}',
+            self.server(),
+            self.database(),
+            self.credentials().userid(),
+            self.credentials().password())
+        raw_cnxn_string = '%r'%cnxn_string  # cast to raw string literal
+        self.cnxn = pyodbc.connect(raw_cnxn_string[1:-1])
 
     def process_signals(self, signals):
         for signal in signals:
             pass
         self.notify_signals(signals)
+
+    def stop(self):
+        super().stop()
+        self.cnxn.close()
--- tests/test_mssql_block.py
+++ tests/test_mssql_block.py
@@ -1,3 +1,4 @@
+from unittest.mock import MagicMock, patch
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
@@ -6,10 +7,22 @@ from ..mssql_block import MSSQL
 
 class TestMSSQL(NIOBlockTestCase):
 
-    def test_process_signals(self):
+    _host = 'host'
+    _db = 'db'
+    _uid = 'user'
+    _pw = 'pw'
+    _driver = '{ODBC Driver 13 for SQL Server}'
+    config = {
+        'server': 'host',
+        'database': 'db',
+        'credentials': {'userid': _uid, 'password': _pw}}
+
+    @patch(MSSQL.__module__ + '.pyodbc')
+    def test_process_signals(self, mock_odbc):
+        cnxn = mock_odbc.connect.return_value = MagicMock()
         """Signals pass through block unmodified."""
         blk = MSSQL()
-        self.configure_block(blk, {})
+        self.configure_block(blk, self.config)
         blk.start()
         blk.process_signals([Signal({"hello": "nio"})])
         blk.stop()
@@ -17,3 +30,15 @@ class TestMSSQL(NIOBlockTestCase):
         self.assertDictEqual(
             self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
             {"hello": "nio"})
+        mock_odbc.connect.assert_called_once_with(
+            'DRIVER={};'
+            'SERVER={};'
+            'DATABASE={};'
+            'UID={};'
+            'PWD={}'.format(
+                self._driver,
+                self._host,
+                self._db,
+                self._uid,
+                self._pw))
+        cnxn.close.assert_called_once()
