409ff108d46f91c0bb7ca0d3b65daf89bbe7c1d2
Tom Young <tyoung@n.io>
2017-12-19 14:34:37 -0700

    query greenbar

--- mssql_block.py
+++ mssql_block.py
@@ -1,5 +1,6 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty, StringProperty, PropertyHolder, ObjectProperty
+from nio.properties import VersionProperty, StringProperty, PropertyHolder, ObjectProperty, Property
+from nio.signal.base import Signal
 import pyodbc
 
 class Credentials(PropertyHolder):
@@ -13,6 +14,7 @@ class MSSQL(Block):
     server = StringProperty(title='Server')
     database = StringProperty(title='Database')
     credentials = ObjectProperty(Credentials, title='Connection Credentials')
+    query = Property(title='Query', default='SELECT * from {{ $table }}')
 
     def __init__(self):
         super().__init__()
@@ -25,20 +27,24 @@ class MSSQL(Block):
             'SERVER={};'
             'DATABASE={};'
             'UID={};'
-            'PWD={}')
-        cnxn_string = cnxn_string.format(
-            '{ODBC Driver 13 for SQL Server}',
-            self.server(),
-            self.database(),
-            self.credentials().userid(),
-            self.credentials().password())
+            'PWD={}').format(
+                '{ODBC Driver 13 for SQL Server}',
+                self.server(),
+                self.database(),
+                self.credentials().userid(),
+                self.credentials().password())
         raw_cnxn_string = '%r'%cnxn_string  # cast to raw string literal
-        self.cnxn = pyodbc.connect(raw_cnxn_string[1:-1])
+        self.cnxn = pyodbc.connect(raw_cnxn_string[1:-1]) # strip extra quotes apparently
 
     def process_signals(self, signals):
+        output_signals = []
+        cursor = self.cnxn.cursor()
         for signal in signals:
-            pass
-        self.notify_signals(signals)
+            rows = cursor.execute(self.query(signal)).fetchall()
+            for row in rows:
+                signal_dict = {key: getattr(row, key) for key in dir(row) if not key.startswith('__')}
+                output_signals.append(Signal(signal_dict))
+        self.notify_signals(output_signals)
 
     def stop(self):
         super().stop()
--- tests/test_mssql_block.py
+++ tests/test_mssql_block.py
@@ -5,6 +5,14 @@ from nio.testing.block_test_case import NIOBlockTestCase
 from ..mssql_block import MSSQL
 
 
+class RowObject(object):
+
+    def __init__(self, a, b, c):
+        super().__init__()
+        self.a = a
+        self.b = b
+        self.c = c
+
 class TestMSSQL(NIOBlockTestCase):
 
     _host = 'host'
@@ -15,21 +23,35 @@ class TestMSSQL(NIOBlockTestCase):
     config = {
         'server': 'host',
         'database': 'db',
-        'credentials': {'userid': _uid, 'password': _pw}}
+        'credentials': {'userid': _uid, 'password': _pw},
+        'query': 'SELECT * from {{ $table }}'}
 
     @patch(MSSQL.__module__ + '.pyodbc')
     def test_process_signals(self, mock_odbc):
-        cnxn = mock_odbc.connect.return_value = MagicMock()
+        self.maxDiff = None
+        mock_cnxn = mock_odbc.connect.return_value = MagicMock()
+        mock_cursor = mock_cnxn.cursor.return_value = MagicMock()
+        mock_cnxn.cursor.return_value.execute.return_value = mock_cursor
+        mock_cursor.fetchall.return_value = [
+            RowObject(a=1.0, b=1.1, c=1.2),
+            RowObject(a=2.0, b=2.1, c=2.2),
+            RowObject(a=3.0, b=3.1, c=3.2)]
         """Signals pass through block unmodified."""
         blk = MSSQL()
         self.configure_block(blk, self.config)
         blk.start()
-        blk.process_signals([Signal({"hello": "nio"})])
+        blk.process_signals([Signal({'table': 'foo'})])
         blk.stop()
-        self.assert_num_signals_notified(1)
+        self.assert_num_signals_notified(3)
         self.assertDictEqual(
             self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "nio"})
+            {'a': 1.0, 'b': 1.1, 'c': 1.2})
+        self.assertDictEqual(
+            self.last_notified[DEFAULT_TERMINAL][1].to_dict(),
+            {'a': 2.0, 'b': 2.1, 'c': 2.2})
+        self.assertDictEqual(
+            self.last_notified[DEFAULT_TERMINAL][2].to_dict(),
+            {'a': 3.0, 'b': 3.1, 'c': 3.2})
         mock_odbc.connect.assert_called_once_with(
             'DRIVER={};'
             'SERVER={};'
@@ -41,4 +63,6 @@ class TestMSSQL(NIOBlockTestCase):
                 self._db,
                 self._uid,
                 self._pw))
-        cnxn.close.assert_called_once()
+        mock_cnxn.cursor.assert_called_once()
+        mock_cnxn.cursor.return_value.execute.assert_called_once_with('SELECT * from foo')
+        mock_cnxn.close.assert_called_once()
