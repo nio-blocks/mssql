d72c5d13b6f2d4d8449beeb5e4dece059825527b
Jake Snell <jsnell@n.io>
2018-01-11 13:44:48 -0700

    Add the MSSQLDelete block and test

---
+++ tests/test_mssql_delete_block.py
@@ -0,0 +1,56 @@
+from unittest.mock import MagicMock, patch
+from nio.block.terminals import DEFAULT_TERMINAL
+from nio.signal.base import Signal
+from nio.testing.block_test_case import NIOBlockTestCase
+from ..mssql_base import MSSQLBase
+from ..mssql_delete_block import MSSQLDelete
+
+
+class TestMSSQL(NIOBlockTestCase):
+
+    _host = 'host'
+    _port = 314
+    _db = 'db'
+    _uid = 'user'
+    _pw = 'pw'
+    _driver = '{ODBC Driver 13 for SQL Server}'
+    config = {
+        'server': _host,
+        'port': _port,
+        'database': _db,
+        'credentials': {'userid': _uid, 'password': _pw},
+        'modify': 'DELETE * from {{ $table }}'}
+
+    @patch(MSSQLBase.__module__ + '.pyodbc')
+    def test_process_signals(self, mock_odbc):
+        mock_cnxn = mock_odbc.connect.return_value = MagicMock()
+        mock_cursor = mock_cnxn.cursor.return_value = MagicMock()
+        mock_cursor.execute.return_value = mock_cursor
+        mock_cursor.rowcount = 5
+
+        blk = MSSQLDelete()
+        self.configure_block(blk, self.config)
+        blk.start()
+        blk.process_signals([Signal({'table': 'testTable'})])
+        blk.stop()
+        self.assert_num_signals_notified(1)
+        self.assertDictEqual(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+            {'Rows deleted': 5})
+        mock_odbc.connect.assert_called_once_with(
+            'DRIVER={};'
+            'PORT={};'
+            'SERVER={};'
+            'DATABASE={};'
+            'UID={};'
+            'PWD={}'.format(
+                self._driver,
+                self._port,
+                self._host,
+                self._db,
+                self._uid,
+                self._pw))
+        self.assertEqual(mock_cnxn.cursor.call_count, 1)
+        mock_cursor.execute.assert_called_once_with('DELETE * from testTable')
+        self.assertEqual(mock_cursor.close.call_count, 1)
+        self.assertEqual(mock_cnxn.close.call_count, 1)
